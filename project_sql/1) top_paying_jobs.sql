ada295b503c3fef661c639824277465a59cfcffc
realYungi <realyungijeongprimary@gmail.com>
2024-03-18 16:48:33 +0900

    Analyzed the top 10 paying companies for the 'Data Analyst Role'

---
+++ project_sql/1) top_paying_jobs.sql
@@ -0,0 +1,22 @@
+-- What are the top paying data analyst jobs?
+-- Identify the top 10 highest paying Data Analyst roles that are available remotely
+-- include salaries without null values
+-- offer insight for employees seeking for the job
+
+SELECT 
+companies.name,
+jobs.job_title_short,
+jobs.salary_year_avg
+
+
+FROM job_postings_fact AS jobs
+LEFT JOIN company_dim AS companies ON jobs.company_id = companies.company_id
+
+WHERE 
+jobs.salary_year_avg IS NOT NULL
+AND jobs.job_location = 'Anywhere'
+AND jobs.job_title_short = 'Data Analyst'
+
+ORDER BY jobs.salary_year_avg DESC
+LIMIT 10;
+
